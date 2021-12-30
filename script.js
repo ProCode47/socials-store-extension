2abc1dbf0ad1e0f8a097ece4cfa055f6a8d2a7ce
ProCode47 <iloenyenwavictor@gmail.com>
2021-12-30 15:45:18 +0100

    done

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-
-const elements = document.getElementsByClassName('.add');
+const elements = document.getElementsByClassName('add');
+const submit = document.getElementById("submit");
+const social = document.getElementById("social");
+const body = document.querySelector("body");
 const modal = document.getElementById("modal");
-const close = document.getElementById("close")
+const close = document.getElementById("close");
+let targetSocial;
 
-console.log(elements)
 
-elements.forEach(element => {
-   element.addEventListner('click', (e)=>{
-       modal.classList.add("flex");
-       console.log("damn")
-   });
-});
-close.addEventListener('click', () => {
+// let profiles = {}
+// localStorage.setItem('profiles', JSON.stringify(profiles));
+let saved = JSON.parse(localStorage.getItem('profiles'));
+
+Object.keys(saved).forEach(value => {
+    let target = document.getElementById(`${value}`);
+    target.innerHTML = saved[value];
+  });
+
+
+const showModal = (e) => {
+    modal.classList.add("flex");
+    body.classList.add("fixed");
+    targetSocial = e.target.id
+}
+const closeModal = () => {
     modal.classList.remove("flex");
     modal.classList.add("none");
+    body.classList.remove("fixed");
+    social.value = "";
+
+}
+
+[...elements].forEach(element => {
+    element.addEventListener('click',showModal)
+});
+
+close.addEventListener('click', () => {
+    closeModal()
+
+})
+submit.addEventListener("click", (e) => {
+    const clipboard = document.getElementById(`${targetSocial}`);  
+    const socialURL = social.value;
+    clipboard.innerText = socialURL;
+    let profiles = JSON.parse(localStorage.getItem('profiles'));
+    profiles[`${targetSocial}`] = socialURL
+    localStorage.setItem('profiles', JSON.stringify(profiles));
+    closeModal();
 })
